790bc25dc80b2e1be6bfe1b87bd303e88c937539
hotheadryann <ryan_williams@envisionacademy.org>
2019-10-23 01:28:00 +0000

    i got my words to do pig latin and made the first letter go to the end and add ay

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-$( "document" ).ready(function() {
-
-
-
+$( "document" ).ready(function(){
+$("#button").click(function(){
+    var userInput = $(".input").val();
+    var piglatinWord = piglatinizer(userInput);
+    $(".output").text(piglatinWord);
+    });
 });
+
+
+function piglatinizer(userInput){
+    return userInput.slice(1) + userInput[0] + "ay";
+}
